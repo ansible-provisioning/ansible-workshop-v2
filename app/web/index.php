56f8b7b5dc5bef0df7ded8be3465c833de6f8fc6
Jeroen Hoekx <jeroen@hoekx.be>
2014-02-03 11:19:56 +0100

    Use the root MySQL login.

--- app/web/index.php
+++ app/web/index.php
@@ -2,7 +2,7 @@
 <title>Ansible Workshop</title>
 
 <?php
-$db = new PDO('mysql:host=localhost;dbname=ws');
+$db = new PDO('mysql:host=localhost;dbname=ws', 'root');
 
 $result = $db->query('SELECT Message FROM Content');
 foreach($result as $row) {

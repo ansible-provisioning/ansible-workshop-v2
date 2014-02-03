507901ee870745f2d6552169b9f2e90c889795bb
Jeroen Hoekx <jeroen@hoekx.be>
2014-02-03 20:20:44 +0100

    Update hosts file.

--- app/web/index.php
+++ app/web/index.php
@@ -2,7 +2,7 @@
 <title>Ansible Workshop</title>
 
 <?php
-$db = new PDO('mysql:host=localhost;dbname=ws', 'root');
+$db = new PDO('mysql:host={{ hostvars["vm-web"]["ansible_ssh_host"] }};dbname=ws', 'ws');
 
 $result = $db->query('SELECT Message FROM Content');
 foreach($result as $row) {
